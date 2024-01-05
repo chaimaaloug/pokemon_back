6324c69f8841fbbdd760d2f565c2be0b1354a6d5
CHAIMAA LOUGSARI <49941834+chaimaaloug@users.noreply.github.com>
2024-01-05 16:09:49 +0100

    update back api

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
