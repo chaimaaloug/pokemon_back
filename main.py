db40b87f38a6744089a8991f65e50ed3a0a570e8
Pierre-Beuselinck <pierrebeuselinck2@gmail.com>
2024-01-12 17:16:00 +0100

    clean

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 # Enable CORS for all origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app = FastAPI()
 
 @app.post("/predict")
