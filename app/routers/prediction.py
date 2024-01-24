404f8a06b20bd1d651fe97ca25994b0237f5a3e8
CHAIMAA LOUGSARI <49941834+chaimaaloug@users.noreply.github.com>
2024-01-24 22:30:16 +0100

    display pokemon details and prediction result

--- app/routers/prediction.py
+++ app/routers/prediction.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
 import pandas as pd
 
 router = APIRouter()
@@ -22,33 +22,57 @@ def get_pokemon_types():
 def get_pokemon_names():
     return {"pokemon_names": pokemon_names}
 
+#Récupérer les informations d'un pokémon
+@router.get("/get_pokemon_details/{pokemon_name}")
+def get_pokemon_details(pokemon_name: str):
+
+    pokemon_data = df_pokemons[df_pokemons['name'] == pokemon_name.lower()]
+
+    if not pokemon_data.empty:
+        def convert_numpy_types(value):
+            if isinstance(value, np.int64):
+                return int(value)
+            return value
+
+        pokemon_details = {
+            "Name": pokemon_data.iloc[0]['name'],
+            "Type 1": pokemon_data.iloc[0]['type1'],
+            "HP": convert_numpy_types(pokemon_data.iloc[0]['hp']),
+            "Attack": convert_numpy_types(pokemon_data.iloc[0]['atk']),
+            "Defense": convert_numpy_types(pokemon_data.iloc[0]['def']),
+            "Special Attack": convert_numpy_types(pokemon_data.iloc[0]['spatk']),
+            "Special Defense": convert_numpy_types(pokemon_data.iloc[0]['spdef']),
+            "Speed": convert_numpy_types(pokemon_data.iloc[0]['speed']),
+            "Height": convert_numpy_types(pokemon_data.iloc[0]['height']),
+            "Weight": convert_numpy_types(pokemon_data.iloc[0]['weight']),
+            "Abilities": convert_numpy_types(pokemon_data.iloc[0]['abilities']),
+            "Description": convert_numpy_types(pokemon_data.iloc[0]['desc'])
+        }
+
+        return {"pokemon_details": pokemon_details}
+    else:
+        raise HTTPException(status_code=404, detail=f"Pokémon not found: {pokemon_name}")
+
 @router.post("/send_prediction_result")
 def send_selected_pokemons(data: dict):
     type1 = data.get("type1")
     type2 = data.get("type2")
 
-    # Appeler la fonction pour comparer les Pokémon
     result = compare_pokemons(type1, type2)
-
-    # Retourner la propriété 'result' dans la réponse
     return {"message": f"Types sélectionnés : {type1}, {type2}", "result": result.get("message", "")}
 
-
 def compare_pokemons(type1: str, type2: str):
-    # Convertir les noms de Pokémon en minuscules
+
     type1 = type1.lower()
     type2 = type2.lower()
 
-    # Sélectionner les données noms des deux Pokémon
     pokemon1_data = df_pokemons[df_pokemons['name'] == type1]
     pokemon2_data = df_pokemons[df_pokemons['name'] == type2]
 
     if not pokemon1_data.empty and not pokemon2_data.empty:
-        # Extraire les caractéristiques pertinentes pour la comparaison
         total_pokemon1 = pokemon1_data.iloc[0]['total']
         total_pokemon2 = pokemon2_data.iloc[0]['total']
 
-        # Comparaison des caractéristiques
         if total_pokemon1 > total_pokemon2:
             message = f"{type1} est potentiellement plus fort que {type2}"
         elif total_pokemon1 < total_pokemon2:
